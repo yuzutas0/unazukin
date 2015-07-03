c938bf74f87d52d2a473894165cfa52b2ab2919e
yuzutas0 <sho.y0921@gmail.com>
2015-07-03 19:17:56 +0900

    reset test

---
+++ SimpleChat/LicenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  LicenceViewController.h
+//  UnazukinChan
+//
+//  Created by sy on 2015/07/03.
+//  Copyright (c) 2015年 classmethod, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LicenceViewController : UIViewController
+
+@end
